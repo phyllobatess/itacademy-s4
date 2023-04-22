ef1f5d1bc0d0f125d0f11c1e9ef15df48cd05c8d
Ramón Diaz <ramon.diazguerra@gmail.com>
2023-04-22 17:42:08 +0200

    Ejercicio 1, Ejercicio 2 y Ejercicio 3

--- src/films.js
+++ src/films.js
@@ -1,46 +1,45 @@
 // Exercise 1: Get the array of all directors.
+// npm run test:watch      : Para ejecutar los tests
 function getAllDirectors(array) {
-  let result;
-  console.log("EXERCICE 1 ->", result);
+  let result = [];
+  result = array.map((e) => e.director);
+  console.log('EXERCICE 1 ->', result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = [];
+  result = array.filter((e) => e.director == director);
+  console.log('EXERCISE 2', result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let arrayDirector = getMoviesFromDirector(array, director);
+  let sumaArrayDirector = arrayDirector.reduce(
+    (sum, currentValue) => sum + currentValue.score,
+    0
+  );
+  let average = Number((sumaArrayDirector / arrayDirector.length).toFixed(2));
+  return average;
 }
 
-// Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
-  
-}
+// Exercise 4:  Alphabetic order by title
+function orderAlphabetically(array) {}
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
-}
+function orderByYear() {}
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
+function moviesAverageByCategory() {}
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
+function hoursToMinutes() {}
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
-}
-
-
+function bestFilmOfYear() {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -53,6 +52,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     moviesAverageByCategory,
     hoursToMinutes,
-    bestFilmOfYear,
+    bestFilmOfYear
   };
 }
